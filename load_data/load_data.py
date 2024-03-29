d30b97e2a783892935f14a9bb81fd6b61e2616e7
ulrikisdahl <uisdahl@gmail.com>
2024-03-29 22:40:17 +0100

    feat: add data loader

---
+++ load_data/load_data.py
@@ -0,0 +1,55 @@
+from torchvision import transforms 
+from torch.utils.data import DataLoader
+import numpy as np
+import torchvision
+import torch
+import matplotlib.pyplot as plt
+
+
+IMG_SIZE = 64
+BATCH_SIZE = 128
+
+
+def load_transformed_dataset():
+    data_transforms = [
+        transforms.Resize((IMG_SIZE, IMG_SIZE)),
+        #transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(), # Scales data into [0,1] 
+        transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1] 
+    ]
+    data_transform = transforms.Compose(data_transforms)
+
+    train = torchvision.datasets.Flowers102(root=".", download=True, 
+                                         transform=data_transform)
+
+    test = torchvision.datasets.Flowers102(root=".", download=True, 
+                                         transform=data_transform, split='test')
+    return torch.utils.data.ConcatDataset([train, test])
+
+
+def show_tensor_image(image):
+    reverse_transforms = transforms.Compose([
+        transforms.Lambda(lambda t: (t + 1) / 2),
+        transforms.Lambda(lambda t: t.permute(1, 2, 0)), # CHW to HWC
+        transforms.Lambda(lambda t: t * 255.),
+        transforms.Lambda(lambda t: t.numpy().astype(np.uint8)),
+        transforms.ToPILImage(),
+    ])
+
+    # Take first image of batch
+    if len(image.shape) == 4:
+        image = image[0, :, :, :] 
+    plt.imshow(reverse_transforms(image))
+
+
+data = load_transformed_dataset()
+dataloader = DataLoader(data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+
+
+def get_data_loader(path: str):
+    """
+        Returns the a torch data loader with the images in range [-1, 1]
+    """
+    data = load_transformed_dataset()
+    dataloader = DataLoader(data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+    return dataloader
